0a3f47cbf8768b54d8f5aa424f9503b0810fe835
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 21:41:27 -0400

    a lot of hard coded values but hey... its working!

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import LemonZesting from "/jpg/lemonZesting.jpeg";
 const Hero = () => {
   return (
-    <div className="max-w-7xl w-full h-[95svh] lg:h-[85svh] mx-auto bg-yellow-200">
+    <div className="max-w-7xl w-full h-[100svh] lg:h-[85svh] mx-auto bg-yellow-200">
       <div className="px-8 grid lg:grid-cols-3 items-center justify-center lg:justify-between">
         <div className="lg:col-span-1">
           <h2 className="font-bold text-7xl">Some kind of heading</h2>
@@ -11,10 +11,12 @@ const Hero = () => {
           </p>
           <button className="">Reserve Now</button>
         </div>
-        <div className="lg:col-span-2 order-1 lg:order-2 my-4 mx-auto lg:m-0 h-[95svh] lg:h-[85svh] flex flex-col lg:items-end">
-          <img className=" w-auto h-full object-contain" src={LemonZesting} />
+        <div className="lg:col-span-2 -order-1 lg:order-2 my-4 mx-auto lg:m-0 h-full flex flex-col lg:items-end">
+          <img
+            className="w-auto lg:max-h-[85vh]  max-h-[450px] sm:max-h-[75vh] h-auto object-contain"
+            src={LemonZesting}
+          />
         </div>
-        {/* <div className="lg:col-span-2 -order-1 lg:order-2"></div> */}
       </div>
     </div>
   );
