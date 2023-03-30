6869e5f590cb82c2f2348ba57d004046ef05666c
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 20:24:08 -0400

    began writing hero based on a previous tutorial knowledge, realized i ddont have any assets!

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import reactLogo from "./assets/react.svg";
+import Hero from "./components/Hero";
 import Test from "./components/Test";
 import viteLogo from "/vite.svg";
 
 function App() {
   return (
-    <div className="App">
-      <Test />
-      <div>hello</div>
+    <div className="bg-slate-100">
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="max-w-7xl w-full h-[85svh] mx-auto bg-yellow-200 grid lg:grid-cols-3">
+      <div className="lg:grid-cols-1">
+        <h2 className="font-bold text-7xl">Some kind of heading</h2>
+        <p className="text-3xl">
+          some kind of subtitle that is a little bit longer
+        </p>
+        <button className="">Reserve Now</button>
+      </div>
+      <img />
+    </div>
+  );
+};
+
+export default Hero;
