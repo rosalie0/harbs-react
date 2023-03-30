9322a87dfeddf0216bb0877a84dd94f2cde684ab
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 21:28:36 -0400

    made the 2 cols a flex

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import LemonZesting from "/jpg/lemonZesting.jpeg";
 const Hero = () => {
   return (
     <div className="max-w-7xl w-full h-[95svh] lg:h-[85svh] mx-auto bg-yellow-200">
-      <div className="px-8 grid lg:grid-cols-3 items-center justify-center">
+      <div className="px-8 grid lg:grid-cols-3 items-center justify-center lg:justify-between">
         <div className="lg:col-span-1">
           <h2 className="font-bold text-7xl">Some kind of heading</h2>
           <p className="text-3xl">
@@ -11,10 +11,9 @@ const Hero = () => {
           </p>
           <button className="">Reserve Now</button>
         </div>
-        <img
-          className="lg:col-span-2 -order-1 lg:order-2 h-[350px] sm:h-[70svh] lg:h-[85svh] mx-auto lg:m-0"
-          src={LemonZesting}
-        />
+        <div className="lg:col-span-2 order-1 lg:order-2 my-4 mx-auto lg:m-0 h-[95svh] lg:h-[85svh] flex flex-col lg:items-end">
+          <img className=" w-auto h-full object-contain" src={LemonZesting} />
+        </div>
         {/* <div className="lg:col-span-2 -order-1 lg:order-2"></div> */}
       </div>
     </div>
