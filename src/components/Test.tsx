ef95274ddeeeaa370532169205d30d205a482269
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-05 15:59:28 -0400

    lots of sample text written, with notes.

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -2,33 +2,97 @@ import React from "react";
 
 function Test() {
   return (
-    <div className="p-12">
-      <p className="text-harbs-brown bg-harbs-yellow">hello harbs</p>
-      <p className="">
+    <div className="p-12 text-harbs-brown bg-harbs-yellow">
+      <p className="uppercase font-cormorant-garamond text-3xl font-bold">
+        harbs, delicious fresh cake **
+      </p>
+      <p className="uppercase font-taviraj text-3xl">
+        harbs, delicious fresh cake
+      </p>
+      <p className="uppercase font-crimson-pro text-3xl">
+        harbs, delicious fresh cake
+      </p>
+      <p className="font-taviraj font-extrabold">
         To borrow money on the credit of the supreme and inferior Courts, shall
         hold his Office.
       </p>
-      <p className="">
+      <p className="font-crimson-pro font-light">
         During the Term of four Years, and, together with the Objections, to the
         other Mode of Ratification may be proposed by the United States: And no
         Person shall be a Majority of each shall constitute a Quorum to do
         Business. In every Case, after the Choice of the first Article. Which
         Day shall be sent, together with the Indian Tribes.
       </p>
-      <p>
-        Note: Superseded by the Eleventh Amendment. To promote the Progress of
-        Science and useful Arts, by securing for limited Times to Authors and
-        Inventors the exclusive Right to their respective Numbers, which shall
-        be a Law, in like Manner chuse the President. To borrow money on the
-        credit of the United States of America the Twelfth. Representatives and
-        direct Taxes shall be determined by adding to the whole Number of Votes,
-        then the House of Representatives. To raise and support Armies, but no
-        Appropriation of Money to that Office who shall not be an Inhabitant of
-        the same throughout the United States. And all other Officers of the
-        Legislatures of three fourths of the Persons voting for and against the
-        Bill shall not be an Inhabitant of the Writ of Habeas Corpus shall not
-        have attained to the supreme Court.
-      </p>
+      <hr className="border-harbs-brown p-4"></hr>
+      <>
+        <h2 className="font-cormorant-garamond text-5xl italic">
+          Cormorant Garamond.
+        </h2>
+        <p className="font-cormorant-garamond text-lg">
+          Perhaps used for 'subtitles' ? like footer copyright. A bit hard to
+          read unless made larger? Feels like a fancy restaraunt menu's font
+          which I like, though.
+        </p>
+        <p className="font-cormorant-garamond">$200.96</p>
+        <p className="font-cormorant-garamond">
+          Note: Superseded by the Eleventh Amendment. To promote the Progress of
+          Science and useful Arts, by securing for limited Times to Authors and
+          Inventors the exclusive Right to their respective Numbers, which shall
+          be a Law, in like Manner chuse the President. To borrow money on the
+          credit of the United States of America the Twelfth. Representatives
+          and direct Taxes shall be determined by adding to the whole Number of
+          Votes, then the House of Representatives. To raise and support Armies,
+          but no Appropriation of Money to that Office who shall not be an
+          Inhabitant of the same throughout the United States. And all other
+          Officers of the Legislatures of three fourths of the Persons voting
+          for and against the Bill shall not be an Inhabitant of the Writ of
+          Habeas Corpus shall not have attained to the supreme Court.
+        </p>
+      </>
+      <hr className="border-harbs-brown p-4"></hr>
+      <>
+        <h2 className="font-crimson-pro text-5xl italic">Crimson Pro.</h2>
+        <p className="font-crimson-pro text-lg">
+          Feels a bit cuter. chunkier? more square. While I don't love it I
+          think it is easier to read.
+        </p>
+        <p className="font-crimson-pro">$200.96</p>
+        <p className="font-crimson-pro">
+          Note: Superseded by the Eleventh Amendment. To promote the Progress of
+          Science and useful Arts, by securing for limited Times to Authors and
+          Inventors the exclusive Right to their respective Numbers, which shall
+          be a Law, in like Manner chuse the President. To borrow money on the
+          credit of the United States of America the Twelfth. Representatives
+          and direct Taxes shall be determined by adding to the whole Number of
+          Votes, then the House of Representatives. To raise and support Armies,
+          but no Appropriation of Money to that Office who shall not be an
+          Inhabitant of the same throughout the United States. And all other
+          Officers of the Legislatures of three fourths of the Persons voting
+          for and against the Bill shall not be an Inhabitant of the Writ of
+          Habeas Corpus shall not have attained to the supreme Court.
+        </p>
+      </>
+      <>
+        <h2 className="font-taviraj text-5xl italic">Taviraj.</h2>
+        <p className="font-taviraj text-lg">
+          Feels like an inbetween. It feels less chunky than crimson pro.
+        </p>
+        <p className="font-taviraj">$200.96</p>
+        <p className="font-taviraj leading-loose">
+          Note: Superseded by the Eleventh Amendment. To promote the Progress of
+          Science and useful Arts, by securing for limited Times to Authors and
+          Inventors the exclusive Right to their respective Numbers, which shall
+          be a Law, in like Manner chuse the President. To borrow money on the
+          credit of the United States of America the Twelfth. Representatives
+          and direct Taxes shall be determined by adding to the whole Number of
+          Votes, then the House of Representatives. To raise and support Armies,
+          but no Appropriation of Money to that Office who shall not be an
+          Inhabitant of the same throughout the United States. And all other
+          Officers of the Legislatures of three fourths of the Persons voting
+          for and against the Bill shall not be an Inhabitant of the Writ of
+          Habeas Corpus shall not have attained to the supreme Court.
+        </p>
+      </>
     </div>
   );
 }
