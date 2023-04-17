41d7ef8c1ac9d6c6a1451c4ac81d312e5b4300f3
Stephen Werbeck <SWerbeck@yahoo.com>
2023-04-17 14:03:47 -0400

    vertical centered side images

--- src/components/locations/LocationCarousel.tsx
+++ src/components/locations/LocationCarousel.tsx
@@ -5,10 +5,29 @@ import {RxDotFilled, RxDot} from 'react-icons/rx'
 
 const LocationCarousel = () => {
 
+    /* make an array of objects with image file paths as the values as pass in as prop 
+     
+    const slides = [
+    {url: ""},
+    {url: ""},
+    {url: ""},
+    {url: ""}
+  ]   
+    
+    
+    */
 const slides = [
     {url: "/locations/chelsea/harbs01.jpg"},
     {url: "/locations/chelsea/harbs02.jpg"},
-    {url: "/locations/chelsea/harbs03.jpg"}
+    {url: "/locations/chelsea/harbs03.jpg"},
+    {url: "/locations/chelsea/harbs04.jpg"},
+    {url: "/locations/chelsea/harbs05.jpg"},
+    {url: "/locations/chelsea/harbs06.jpg"},
+    {url: "/locations/chelsea/harbs07.jpg"},
+    {url: "/locations/chelsea/harbs08.jpg"},
+    {url: "/locations/chelsea/harbs09.jpg"},
+    {url: "/locations/chelsea/harbs10.jpg"},
+    {url: "/locations/chelsea/harbs11.jpg"}
 ]    
 
 const [currentIndex, setCurrentIndex] = useState(0)
@@ -30,20 +49,20 @@ const goToSlide = (slideIndex: number) => {
 
 
   return (
-    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
+    <div className='border-4 border-harbs-brown max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
     
-        <div className="flex flex-row justify-center lg:justify-around">
-        <img src={currentIndex === 0 ? `${slides[slides.length-1].url}` : `${slides[currentIndex-1].url}`} className="w-auto  min-h-[50px] max-h-[300px]"></img>
+        <div className="flex flex-row items-center content-center lg:justify-around">
+        <img src={currentIndex === 0 ? `${slides[slides.length-1].url}` : `${slides[currentIndex-1].url}`} className="w-auto  min-h-[50px] max-h-[300px] opacity-50"></img>
         <img src={`${slides[currentIndex].url}`} className="w-auto  min-h-[80px] max-h-[500px] m-10"></img>
-        <img src={currentIndex === slides.length-1 ? `${slides[0].url}` : `${slides[currentIndex+1].url}`} className="w-auto  min-h-[50px] max-h-[300px]"></img>
+        <img src={currentIndex === slides.length-1 ? `${slides[0].url}` : `${slides[currentIndex+1].url}`} className="w-auto  min-h-[50px] max-h-[300px] opacity-50"></img>
         
         </div>
-        <div className="absolute top-[50%] -translate-x-0 tranlate-y-[50%] left-5 text2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"><BsChevronCompactLeft onClick={prevSlide} size={30}/></div>
-        <div className="absolute top-[50%] -translate-x-0 tranlate-y-[50%] right-5 text2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"><BsChevronCompactRight onClick={nextSlide} size={30}/></div>
+        <div className="absolute top-[40%] -translate-x-0 tranlate-y-[50%] left-5 text2xl rounded-full p-2 bg-black/50 text-white cursor-pointer"><BsChevronCompactLeft onClick={prevSlide} size={30}/></div>
+        <div className="absolute top-[40%] -translate-x-0 tranlate-y-[50%] right-5 text2xl rounded-full p-2 bg-black/50 text-white cursor-pointer"><BsChevronCompactRight onClick={nextSlide} size={30}/></div>
         <div className="flex top-4 justify-center py-2">
             {slides.map((slide, slideIndex) => (
                 <div key={slideIndex} onClick={() => goToSlide(slideIndex)}>
-                    {slideIndex===currentIndex ? <RxDotFilled /> : <RxDot />}
+                    {slideIndex===currentIndex ? <RxDotFilled fill-red/> : <RxDot />}
                 </div>
                 
                 ))}
