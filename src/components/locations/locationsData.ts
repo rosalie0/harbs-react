7fa5af4e6c5a32f0e00160e38e3d1b2c1e8cebed
Stephen Werbeck <SWerbeck@yahoo.com>
2023-04-17 19:46:30 -0400

    setting up locations with carousel

--- src/components/locations/locationsData.ts
+++ src/components/locations/locationsData.ts
@@ -10,6 +10,21 @@ export interface LocationInterface {
 }
 
 export const chelseaData = {
+  
+  imageUrls:  [
+    {url: "/locations/chelsea/harbs01.jpg"},
+    {url: "/locations/chelsea/harbs02.jpg"},
+    {url: "/locations/chelsea/harbs03.jpg"},
+    {url: "/locations/chelsea/harbs04.jpg"},
+    {url: "/locations/chelsea/harbs05.jpg"},
+    {url: "/locations/chelsea/harbs06.jpg"},
+    {url: "/locations/chelsea/harbs07.jpg"},
+    {url: "/locations/chelsea/harbs08.jpg"},
+    {url: "/locations/chelsea/harbs09.jpg"},
+    {url: "/locations/chelsea/harbs10.jpg"},
+    {url: "/locations/chelsea/harbs11.jpg"}
+],    
+
  
   name: "Chelsea",
   address: "198 9th Ave, New York, NY 10011",
@@ -30,6 +45,20 @@ export const chelseaData = {
 };
 
 export const sohoData = {
+
+  imageUrls:  [
+    {url: "/locations/soho/HARBS_SOHO_01.jpg"},
+    {url: "/locations/soho/HARBS_SOHO_02.jpg"},
+    {url: "/locations/soho/HARBS_SOHO_03.jpg"},
+    {url: "/locations/soho/HARBS_SOHO_04.jpg"},
+    {url: "/locations/soho/HARBS_SOHO_05.jpg"},
+    {url: "/locations/soho/HARBS_SOHO_06.jpg"},
+    {url: "/locations/soho/HARBS_SOHO_07.jpg"},
+    {url: "/locations/soho/HARBS3_1920x1080.jpg"},
+    
+],    
+
+
   name: "Soho",
   address: "465 West Broadway, New York, NY 10012",
   googleMapsUrl: "https://goo.gl/maps/S34KT9by7rr",
