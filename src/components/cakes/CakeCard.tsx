674abfff7858625cb86062a648c6815f18905949
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-24 18:13:11 -0400

    removed not needed code

--- src/components/cakes/CakeCard.tsx
+++ src/components/cakes/CakeCard.tsx
@@ -4,36 +4,29 @@ import AlcoholText from "./AlcoholText";
 import BestSellerBannerOnCard from "./BestSellerBannerOnCard";
 
 const CakeCard = ({ cake }: { cake: CakeInterface }) => {
-  const width = cake.isBestSeller ? "md:w-1/3" : "md:w-1/4";
-
-  const normalClassnames =
-    "flex flex-col items-center bg-white border shadow-md rounded-sm w-auto md:w-1/4 ";
-
-  const larger =
-    "flex flex-col items-center bg-white border shadow-md rounded-sm w-auto md:w-1/3";
   return (
     <div className="flex flex-col items-center bg-white shadow-md rounded-sm w-auto md:w-1/4 h-auto font-crimson-pro">
       {cake.isBestSeller && <BestSellerBannerOnCard />}
-      <div>
-        <div className="p-4 flex flex-col">
-          <img src={cake.imageURL} alt="a photograph of the cake" />
-          <h4 className="font-semibold font-cormorant-garamond text-3xl text-center pt-2">
-            {cake.name}
-          </h4>
+      <div className="p-4 flex flex-col">
+        <img src={cake.imageURL} alt="a photograph of the cake" />
+        <h4 className="font-semibold font-cormorant-garamond text-3xl text-center pt-2">
+          {cake.name}
+        </h4>
 
-          {/* <div className="text-center">
+        {/* Other styles: */}
+        {/* <div className="text-center">
             <h5>Slice | Whole</h5>
             <h5>
               ${cake.slicePrice}.00 | ${cake.wholePrice}.00
             </h5>
           </div> */}
 
-          {/* <div className="self-center">
+        {/* <div className="self-center">
             <h5>Slice: ${cake.slicePrice}.00</h5>
             <h5>Whole: ${cake.wholePrice}.00</h5>
           </div> */}
 
-          {/* <div className="flex flex-nowrap self-center items-center justify-between w-1/2 px-8 ">
+        {/* <div className="flex flex-nowrap self-center items-center justify-between w-1/2 px-8 ">
             <div>
               <p>Slice:</p>
               <p>Whole:</p>
@@ -44,17 +37,16 @@ const CakeCard = ({ cake }: { cake: CakeInterface }) => {
             </div>
           </div> */}
 
-          <div className="self-center">
-            <h5>
-              ${cake.slicePrice} Slice / ${cake.wholePrice} Whole
-            </h5>
-          </div>
-          <p className="self-center text-center max-w-[320px]">
-            {cake.description}
-          </p>
-
-          <AlcoholText alcoholAmount={cake.alcoholAmount} />
+        <div className="self-center">
+          <h5>
+            ${cake.slicePrice} Slice / ${cake.wholePrice} Whole
+          </h5>
         </div>
+        <p className="self-center text-center max-w-[320px]">
+          {cake.description}
+        </p>
+
+        <AlcoholText alcoholAmount={cake.alcoholAmount} />
       </div>
     </div>
   );
