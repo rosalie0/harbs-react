7f9ae3f979ee6b0a7ca131b8a35f1ed36069087a
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-10 16:16:50 -0400

    installed package. made comp based on readme. api key not inserted yet

---
+++ src/components/locations/Map.tsx
@@ -0,0 +1,46 @@
+import React, { memo, useCallback, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const chelseaCenter = {
+  lat: 40.745849,
+  lng: -74.001548,
+};
+const center = chelseaCenter;
+
+const Map = () => {
+  const [map, setMap] = useState(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  });
+
+  const onLoad = useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={chelseaCenter}
+      onLoad={onLoad}
+    ></GoogleMap>
+  ) : (
+    <div></div>
+  );
+};
+
+export default memo(Map);
