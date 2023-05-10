b80291b9511e444ea802c030d8809ee073bf6901
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-10 11:48:19 -0400

    ourstory uses SectionHeader

--- src/components/utils/SectionHeader.tsx
+++ src/components/utils/SectionHeader.tsx
@@ -4,7 +4,7 @@ const SectionHeader = ({ title }: { title: string }) => {
   return (
     <div>
       <h3 className="text-5xl uppercase italic font-cormorant-garamond text-center text-harbs-brown-darkest">
-        Featured Cakes
+        {title}
       </h3>
       <div className="border-b-2 pt-2 border-harbs-brown-darkest w-64 mx-auto" />
     </div>
