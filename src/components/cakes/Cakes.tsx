083483d978eab2d2d9b0b65f48055f4b18907b79
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-10 11:42:24 -0400

    re-usable comp made, in utils folder. 'featured cakes' is now one of these.

--- src/components/cakes/Cakes.tsx
+++ src/components/cakes/Cakes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CakeCard from "./CakeCard";
 import { cakes } from "./cakesData";
 import CakeSectionInfo from "./CakeSectionInfo";
+import SectionHeader from "../utils/SectionHeader";
 
 const Cakes = () => {
   return (
@@ -10,11 +11,7 @@ const Cakes = () => {
       flex flex-col items-center py-12
     bg-harbs-yellow-lightest"
     >
-      <h3 className="text-5xl uppercase italic font-cormorant-garamond text-center text-harbs-brown-darkest">
-        Featured Cakes
-      </h3>
-      <div className="border-b-2 pt-2 border-harbs-brown-darkest w-64 mx-auto" />
-
+      <SectionHeader title={"Featured Cakes"} />
       <CakeSectionInfo />
 
       <div className="flex flex-wrap justify-center items-center gap-4 lg:gap-8 py-12">
